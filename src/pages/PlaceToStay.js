402ad2f4bb5cdc092636da49622f1f6f826298e4
Ajulibe Chiemela <chiemelaajulibe@yahoo.com>
2022-11-14 06:34:39 +0100

    feat:add cards for placesToStay page

--- src/pages/PlaceToStay.js
+++ src/pages/PlaceToStay.js
@@ -1,7 +1,21 @@
 import React from "react";
+import Card from "../components/UI/Card";
+import { places } from "../data/cardData";
+import "./PlaceToStay.scss";
 
 const PlaceToStay = () => {
-  return <div>PlaceToStay</div>;
+  return (
+    <div className="places-page container">
+      <section>filter</section>
+      <section className="places-page-cards">
+        <div className="places-page-cards-inner">
+          {places.map((place) => (
+            <Card image={place.image} />
+          ))}
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default PlaceToStay;
